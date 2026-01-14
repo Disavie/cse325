1adf2ea0961c1e48798a09022c0d486358e0541a
Davis J Stermer <stermerd@compute04.egr.msu.edu>
2026-01-14 18:59:16 -0500

    cse325 proj01 complete i think

---
+++ project1/proj01.cpp
@@ -0,0 +1,45 @@
+// A program calculating address size and memory size from a command line input
+
+#include <iostream>
+#include <cmath>
+#include <cctype>
+
+int main(int argc, char * argv[]){
+//    std::cout << argc << std::endl;
+    bool err = false;
+    int length = 0;
+    if (argc == 2){
+        // Getting length of the address
+        // Also checking for a valid address, must contain alphanumeric character with largest character being F (ascii 72)
+        char *c = argv[1];
+        while(*c != '\0'){
+            //std::cout << int(*c) << std::endl;
+            char ch = std::tolower(*c);
+            if(!std::isdigit(ch) && !(ch >= 'a' && ch <= 'f')){
+                err = true;
+                break;
+            } 
+            if(length >= 8){
+                err = true;
+                break;
+            }
+            length++;
+            c++;
+        }
+    }else{
+        err = true;
+    }
+
+
+    if (!err){
+        int bits = 4 * length;
+        long memSize = pow(2,bits);
+        std::cout << "Address Size: " << bits << " bits" << std::endl;
+        std::cout << "Memory Size: " << memSize << " bytes" << std::endl;
+    }else{
+        // Error case handling
+      std::cout << "Error with argv (empty or invalid)" << std::endl;  
+    }
+    return 0;
+
+}
