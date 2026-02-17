57ae925e8d109c52836b6cd9d29cc3bd6a1b5e60
djs <davisstermer@gmail.com>
2026-02-17 16:50:50 -0500

    threads added, reads inventory.old too

--- project4/proj04.cpp
+++ project4/proj04.cpp
@@ -2,22 +2,45 @@
 #include <vector>
 #include <string>
 #include <pthread.h>
+#include <unistd.h>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
 
-struct Order{
-
-    float price = 0;
-
+struct Object{
+    unsigned int id;
+    float price;
+    unsigned int quantity;
+    string description;
 };
 
+void PrintInventory(vector<Object> &Inventory){
+    // Print objects in Inventory
+    for (size_t i = 0; i < Inventory.size(); i++) {
+        cout << Inventory[i].id << " "
+            << Inventory[i].price << " "
+            << Inventory[i].quantity << " "
+            << Inventory[i].description << endl;
+    }
+}
+
+void * InitProducer(void * arg){
+    cout << '[' << *(int *)arg << ']' << endl;
+    return nullptr;
+}
+
+void * InitConsumer(void * arg){
+    cout << "Consumer!" << endl;
+    return nullptr;
+}
 
 
 int main(int argc, char ** argv){
 
     const int DEFAULTSZ = 10; 
-    vector<Order> Inventory(DEFAULTSZ);
+    vector<Object> Inventory(DEFAULTSZ);
     int numProducer = 1;
 
     for(int i = 0 ; i < argc ; i++){
@@ -43,15 +66,56 @@ int main(int argc, char ** argv){
     cout << "numProducer = " << numProducer << endl;
 
     //init inventory
-    
+    string filename = "testcases/inventory.old";
+    ifstream inventory;
+    inventory.open(filename);
+    string line;
+    int i = 0;
+    while (getline(inventory, line)) {
+        stringstream ss(line);
 
+        int id;
+        double price;
+        int quantity;
+        string description;
 
+        ss >> id >> price >> quantity;
+        getline(ss, description);          // get rest of line as description
+
+        // remove leading space from description
+        if (!description.empty() && description[0] == ' ')
+            description.erase(0, 1);
+
+        Object x;
+        x.id = id;
+        x.price = price;
+        x.quantity = quantity;
+        x.description = description;
+
+        Inventory[i] = x;
+        i++;
+    }
+
+    inventory.close();
+
+    PrintInventory(Inventory);
+    /// Add 1 to account for singular consumer (order fulfiller)
+    pthread_t threads[1+numProducer];
+
+    ///dispatch consumer
+    pthread_create(&threads[0],nullptr,InitConsumer,nullptr);
 
     //init producers
+    vector<int> ids(numProducer);
+    for (int i = 0; i < numProducer; i++) {
+        ids[i] = i;
+        pthread_create(&threads[i + 1], nullptr, InitProducer, &ids[i]);
+    }
 
-
-    for (int i = 0 ; i < numProducers < i++){
-        //dispatch producer thread and init
+    /// Rejoin threads
+    for(int i = 0 ; i < 1+numProducer; i++){
+        /// Rejoin threads
+        pthread_join(threads[i],nullptr);
     }
     return 0;
 }
